2b1459a6ccf2dd7c8954a1a13fe2690f6e7e2460
Facundo Rocha <facundoestudiorocha@gmail.com>
2022-01-03 14:03:50 -0300

    change cors

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const morgan = require("morgan");
 const cors = require("cors");
 const mongodb = require("./config/MongoDB");
 const app = express();
- 
+
 //settings
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 
@@ -22,5 +25,3 @@ app.use("/vehicle", require("./routes/VehicleRoute"));
 app.use("/user", require("./routes/UserRoute"));
 app.use("/company", require("./routes/CompanyRoute"));
 app.use("/brands", require("./routes/VehiclesBrandsRoute"));
-
-
