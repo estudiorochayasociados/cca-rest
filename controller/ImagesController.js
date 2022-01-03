128938839eb917ebc905acd30be8dece4110af0b
Facundo Rocha <facundoestudiorocha@gmail.com>
2022-01-03 14:45:06 -0300

    uploads vehicles files with cloudinary

--- controller/ImagesController.js
+++ controller/ImagesController.js
@@ -1,4 +1,5 @@
 const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
 cloudinary.config({
   cloud_name: "estudio-rocha",
@@ -7,12 +8,35 @@ cloudinary.config({
 });
 
 exports.uploads = (file) => {
-  result = [];
   return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload(file, (data) => {
-      console.log(data);
-      result.push({ url: data.url, id: data.public_id });
+    cloudinary.uploader.upload(file, function (error, result) {
+      resolve(result);
     });
-    resolve(result);
+  });
+};
+
+exports.uploadMany = (files_input) => {
+  return new Promise(async (resolve, reject) => {
+    const uploader = async (path) => await this.uploads(path, "vehicles");
+    const urls = [];
+    const files = files_input;
+
+    for (const file of files) {
+      const { path } = file;
+      const newPath = await uploader(path);
+      urls.push(newPath);
+      fs.unlinkSync(path);
+    }
+
+    resolve(urls);
+  });
+};
+
+exports.deleteAll = (images) => {
+  return new Promise((resolve, reject) => {
+    for (const image of images) {
+      cloudinary.uploader.destroy(image.public_id, function (error, result) {});
+    }
+    resolve(1);
   });
 };
--- routes/VehicleRoute.js
+++ routes/VehicleRoute.js
@@ -4,7 +4,6 @@ const ImagesController = require("../controller/ImagesController");
 const Middleware = require("../config/Middleware");
 const router = express.Router();
 const upload = require("../utils/multer");
-const fs = require("fs");
 
 router.get("/", Middleware.checkToken, async (req, res) => {
   await VehicleController.list(req.body)
@@ -27,8 +26,10 @@ router.get("/:id", Middleware.checkToken, async (req, res) => {
 });
 
 router.delete("/:id", Middleware.checkToken, async (req, res) => {
+  const view = await VehicleController.view(req.params.id);
   await VehicleController.delete(req.params.id)
-    .then((data) => {
+    .then(async (data) => {
+      await ImagesController.deleteAll(view.images);
       res.status(200).json(data);
     })
     .catch((err) => {
@@ -39,47 +40,37 @@ router.delete("/:id", Middleware.checkToken, async (req, res) => {
 router.post(
   "/",
   Middleware.checkToken,
-  upload.array('images',10),
+  upload.array("images", 10),
   async (req, res) => {
-    const uploader = async (path) => await ImagesController.uploads(path,'vehicles');
-
-    const urls = [];
-
-    const files = req.files;
-
-    console.log(files);
-    for(const file of files){
-      const {path} = file;
-      const newPath = await uploader(path);
-      urls.push(newPath);
-      fs.unlinkSync(path);
+    if (req.files) {
+      req.body.images = await ImagesController.uploadMany(req.files);
     }
-
-    res.status(200).json(urls);
-
-
-    // console.log(req.file, req.body);
-    // req.body.images = req.files ? await ImagesController.create(req.files) : [];
-
-    // console.log(req.body);
-    // await VehicleController.create(req.body)
-    //   .then((data) => {
-    //     res.status(200).json(data);
-    //   })
-    //   .catch((err) => {
-    //     res.status(500).json({ error: err });
-    //   });
+    await VehicleController.create(req.body)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
   }
 );
 
-router.put("/:id", Middleware.checkToken, async (req, res) => {
-  await VehicleController.update(req.params.id, req.body)
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err });
-    });
-});
+router.put(
+  "/:id",
+  Middleware.checkToken,
+  upload.array("images", 10),
+  async (req, res) => {
+    if (req.files) {
+      req.body.images = await ImagesController.uploadMany(req.files);
+    }
+    await VehicleController.update(req.params.id, req.body)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
+  }
+);
 
 module.exports = router;
