d52d6bd7bef4af4bf921a33216e26464e3cec65e
Facundo Rocha <facundoestudiorocha@gmail.com>
2022-01-03 11:23:00 -0300

    add companies route/model/controller

--- config/Middleware.js
+++ config/Middleware.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
+const config  = (process.env.MONGO_DB) ? process.env : require('dotenv').config().parsed;
 
 exports.checkToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, "456c103f26656ae321cc8437317174bd", (err, decoded) => {
+    jwt.verify(token, config.JWT, (err, decoded) => {
       if (err) {
         return res.json({ message: "Token inválida" });
       } else {
