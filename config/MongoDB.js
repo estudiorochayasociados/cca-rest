8ad143c42b07939d62d376fb81c495ecce8c86a0
Santi2411 <santi.gonzalez2411@gmail.com>
2022-02-03 10:10:06 -0300

    fix deprecations

--- config/MongoDB.js
+++ config/MongoDB.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose'); 
 const config  = (process.env.MONGO_DB) ? process.env : require('dotenv').config().parsed;
- mongoose.connect('mongodb+srv://'+config.MONGO_USER+':'+config.MONGO_PASS+'@'+config.MONGO_HOST+'/'+config.MONGO_DB+'?retryWrites=true&useNewUrlParser=true&w=majority', function(err, db) {
+ mongoose.connect('mongodb+srv://'+config.MONGO_USER+':'+config.MONGO_PASS+'@'+config.MONGO_HOST+'/'+config.MONGO_DB+'?retryWrites=true&useNewUrlParser=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, 
+ useCreateIndex: true }, function(err, db) {
     if (err) {
         console.log('Unable to connect to the server. Please start the server. Error:', err);
     } else {
--- seeders/VehicleSeeder.js
+++ seeders/VehicleSeeder.js
@@ -1,49 +1,23 @@
-const faker = require("faker");
 const mongodb = require("../config/MongoDB");
-const VehicleController = require("../controller/VehicleController");
-const item = {};
-for (let i = 0; i < 25; i++) {
-    item.color = faker.random.arrayElement(["ROJO", "NEGRO", "BLANCO", "AZUL", "ROSADO", "VIOLETA", "MARINO", "AMARILLO", "GRIS", "VERDE", "ANARANJADO",]);
-    item.description = faker.lorem.words(25);
-    item.direction = faker.random.arrayElement([
-        "ASISTIDA",
-        "HIDRÁULICA",
-        "MECÁNICA",
-    ]);
-    item.transmission = faker.random.arrayElement(["MANUAL", "AUTOMÁTICO"]);
-    item.status = faker.random.arrayElement(["USADO", "NUEVO"]);
-    item.status_publication = faker.random.arrayElement(['PUBLICADO', 'NO PUBLICADO', 'EN REVISIÓN']);
-    item.brand = "PRUEBA 2";
-    item.model = faker.random.arrayElement(["MOTOMEL", "YAMAHA"]);
-    item.regularPrice = faker.random.number({ min: 600000, max: 10000000 });
-    item.resellerPrice = faker.random.number({
-        min: item.regularPrice,
-        max: 10000000,
-    });
-    item.kilometers = faker.random.number({
-        min: 0,
-        max: 100000,
-    });
-    item.doors = faker.random.arrayElement(["2", "3", "4", "5"]);
-    item.fuel = faker.random.arrayElement(["NAFTA", "GASOIL", "DIESEL", "HÍBRIDO", "ELÉCTRICO"]);
-    item.company = faker.random.arrayElement(["61ea981f259f3d1f20a99c25", "61ea9be535432c3994580408", "61ea9c3835432c399458040b", "61ea9c7235432c399458040e", "61ea9d1d35432c3994580411"]);
-    item.images = [
-        {
-            url: faker.image.imageUrl(),
-        },
-        {
-            url: faker.image.imageUrl(),
-        },
-        {
-            url: faker.image.imageUrl(),
-        },
-        {
-            url: faker.image.imageUrl(),
-        },
-    ];
 
-    VehicleController.create(item).then(() => {
-        console.log("CARGADO " + (i + 1))
-    });
+const seeder1 = require("./VehicleSeeder1.js");
+const seeder2 = require("./VehicleSeeder2.js");
+const seeder3 = require("./VehicleSeeder3.js");
+const seeder4 = require("./VehicleSeeder4.js");
+const seeder5 = require("./VehicleSeeder5.js");
+const seeder6 = require("./VehicleSeeder6.js");
+const seeder7 = require("./VehicleSeeder7.js");
+const seeder8 = require("./VehicleSeeder8.js");
+const seeder9 = require("./VehicleSeeder9.js");
+const seeder10 = require("./VehicleSeeder10.js");
 
-}
+seeder1.runSeeder();
+seeder2.runSeeder();
+seeder3.runSeeder();
+seeder4.runSeeder();
+seeder5.runSeeder();
+seeder6.runSeeder();
+seeder7.runSeeder();
+seeder8.runSeeder();
+seeder9.runSeeder();
+seeder10.runSeeder();
